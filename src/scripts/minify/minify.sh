2f87a63a543396fd6326700fe690e294c0e8d18c
Hegesippe Ëspace <Loveangel1337@users.noreply.github.com>
2018-11-10 17:40:16 +0100

    Implement image lazyloading (and update browse script to latest) (#11)

--- src/scripts/minify/minify.sh
+++ src/scripts/minify/minify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_VERSION="2.0.0"
-
+BROWSE_VERSION="2.0.1"
 CURRENTSCRIPT="$(readlink -e $0)"
 SCRIPTNAME="$(basename ${CURRENTSCRIPT})"
 [ -z "${CURRENTSCRIPT}" ] && echo "Unable to find current script name" && exit 1
@@ -14,13 +14,13 @@ cd "${DIR}/../../../"
 
 ASSET_PATH="public/assets"
 
-FOLDERS=("${ASSET_PATH}/js/browse/${CURRENT_VERSION}/" "${ASSET_PATH}/js/leaflet/edit/${CURRENT_VERSION}/" "${ASSET_PATH}/js/leaflet/map/${CURRENT_VERSION}/" "${ASSET_PATH}/js/nav/${CURRENT_VERSION}/" "${ASSET_PATH}/css/browse/${CURRENT_VERSION}/" "${ASSET_PATH}/css/leaflet/edit/${CURRENT_VERSION}/" "${ASSET_PATH}/css/leaflet/map/${CURRENT_VERSION}/" "${ASSET_PATH}/css/nav/${CURRENT_VERSION}/")
+FOLDERS=("${ASSET_PATH}/js/browse/${BROWSE_VERSION}/" "${ASSET_PATH}/js/leaflet/edit/${CURRENT_VERSION}/" "${ASSET_PATH}/js/leaflet/map/${CURRENT_VERSION}/" "${ASSET_PATH}/js/nav/${CURRENT_VERSION}/" "${ASSET_PATH}/css/browse/${CURRENT_VERSION}/" "${ASSET_PATH}/css/leaflet/edit/${CURRENT_VERSION}/" "${ASSET_PATH}/css/leaflet/map/${CURRENT_VERSION}/" "${ASSET_PATH}/css/nav/${CURRENT_VERSION}/")
 
 for p in "${FOLDERS[@]}"; do
     [ ! -d "${p}" ] && mkdir -p "${p}"
 done
 
-uglifyjs src/js/browse/main.js -m > "${ASSET_PATH}/js/browse/${CURRENT_VERSION}/browse.js"
+uglifyjs src/js/browse/main.js -m > "${ASSET_PATH}/js/browse/${BROWSE_VERSION}/browse.js"
 uglifyjs src/js/maps/edit.js -m > "${ASSET_PATH}/js/leaflet/edit/${CURRENT_VERSION}/map.js"
 uglifyjs src/js/maps/show.js -m > "${ASSET_PATH}/js/leaflet/map/${CURRENT_VERSION}/map.js"
 uglifyjs src/js/nav/BurgerKing.js -m > "${ASSET_PATH}/js/nav/${CURRENT_VERSION}/BurgerKing.js"
